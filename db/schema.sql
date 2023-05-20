1280ac46c0220f99e6bdcd958bb326dbe2c9b2df
Valais Girl <Yevette@Hal-71.local>
2023-05-20 17:35:09 -0500

    table setup

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO-INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+    PRIMARY KEY (id),
     
 );
 
@@ -13,18 +14,21 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NUll AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES role(id),
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
-)
+);
