63ab117b5612449e4b8cc31a6198b9def98866bf
Valais Girl <Yevette@Hal-71.local>
2023-05-21 13:05:11 -0500

    could get employee or managers to seed so I separated the seeds

--- db/seed.sql
+++ db/seed.sql
@@ -6,4 +6,3 @@ VALUES ('Service'), ('Engineering'), ('Sales');
 INSERT INTO role (title, salary, department_id)
 VALUES ('Technician', 102000, 1),('Senior Technician', 150000, 1),('Service Coordinator', 180000, 1),('Electrical', 130000, 2),( 'Mechanical',130000, 2), ('Lead Engineer',150000, 2),('Sales Associate',120000, 3),('Sales Manager', 160000, 3)
 
-
